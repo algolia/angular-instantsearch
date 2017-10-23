56df9ea860a584834441e9485be50e337de06c35
iam4x <iam4x@me.com>
2017-10-23 14:11:05 +0200

    docs(demo): add `<ngis-hits-per-page-selector>`

--- examples/demo/app/app.component.ts
+++ examples/demo/app/app.component.ts
@@ -6,6 +6,7 @@ import { NgISInstance } from "angular-instantsearch";
   template: `
     <ngis-instantsearch [config]="config">
       <h1>Angular InstantSearch demo</h1>
+      <ngis-hits-per-page-selector [items]="items"></ngis-hits-per-page-selector>
       <ngis-search-box></ngis-search-box>
       <ngis-clear-all></ngis-clear-all>
       <ngis-hits></ngis-hits>
@@ -23,6 +24,13 @@ export class AppComponent {
   public config = {
     apiKey: "6be0576ff61c053d5f9a3225e2a90f76",
     appId: "latency",
-    indexName: "instant_search"
+    indexName: "instant_search",
+    urlSync: true
   };
+
+  public items = [
+    { value: 10, label: "10 hits per page", default: true },
+    { value: 20, label: "20 hits per page" },
+    { value: 30, label: "30 hits per page" }
+  ];
 }
