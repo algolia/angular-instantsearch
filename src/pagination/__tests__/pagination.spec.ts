2f4810a929b7fe9882e30ccd121c350ab3558a8d
iam4x <iam4x@me.com>
2017-10-26 16:07:06 +0200

    test(widgets): pagination

---
+++ src/pagination/__tests__/pagination.spec.ts
@@ -0,0 +1,110 @@
+import { TestBed } from "@angular/core/testing";
+
+import { NgAisInstantSearchModule } from "../../instantsearch/instantsearch.module";
+import { NgAisPaginationModule } from "../pagination.module";
+import { NgAisPagination } from "../pagination";
+
+import { bem } from "../../utils";
+
+const cx = bem("Pagination");
+
+jest.mock("../../base-widget");
+
+const defaultState = {
+  createURL: jest.fn(),
+  currentRefinement: 0,
+  nbHits: 100,
+  nbPages: 20,
+  refine: jest.fn()
+};
+
+const render = (state?: {}) => {
+  const fixture = TestBed.createComponent(NgAisPagination);
+
+  if (state) {
+    fixture.componentInstance.updateState({ ...defaultState, ...state });
+    fixture.detectChanges();
+  }
+
+  return fixture;
+};
+
+describe("Pagination", () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [NgAisInstantSearchModule.forRoot(), NgAisPaginationModule]
+    })
+  );
+
+  it("renders markup without state", () => {
+    const fixture = render();
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it("renders with pages in state", () => {
+    const fixture = render({});
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it("renders does not uses pagesPadding when nbPages < pagesPadding * 2 + 1", () => {
+    const fixture = render({ nbPages: 5 });
+    const pages = fixture.debugElement.nativeElement.querySelectorAll(
+      "." + cx("item", "page")
+    );
+
+    expect(pages.length).toBe(5);
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it("should refine when clicking a page", () => {
+    const refine = jest.fn();
+    const fixture = render({ refine });
+
+    const el = fixture.debugElement.nativeElement;
+    const [, secondPage] = el.querySelectorAll("." + cx("item", "page"));
+    secondPage.click();
+
+    expect(refine).toHaveBeenCalled();
+    expect(refine).toHaveBeenCalledWith(1);
+
+    fixture.componentInstance.state.currentRefinement = 3;
+    fixture.detectChanges();
+
+    const previous = el.querySelector("." + cx("item", "previousPage"));
+    previous.click();
+
+    expect(refine).toHaveBeenLastCalledWith(2);
+
+    const next = el.querySelector("." + cx("item", "nextPage"));
+    next.click();
+
+    expect(refine).toHaveBeenLastCalledWith(4);
+  });
+
+  it("should display last page button", () => {
+    const refine = jest.fn();
+    const fixture = render({ refine });
+
+    fixture.componentInstance.showLast = true;
+    fixture.detectChanges();
+
+    const lastPage = fixture.debugElement.nativeElement.querySelector(
+      "." + cx("item", "lastPage")
+    );
+
+    lastPage.click();
+
+    expect(refine).toHaveBeenCalled();
+    expect(refine).toHaveBeenCalledWith(defaultState.nbPages);
+  });
+
+  it("should display an header and a footer", () => {
+    const fixture = render({});
+    fixture.componentInstance.header = "Header title";
+    fixture.componentInstance.footer = "Footer title";
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
+  });
+});
