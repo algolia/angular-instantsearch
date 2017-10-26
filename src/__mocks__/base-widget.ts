28b69e24ac377a8a130f879cd2424130b8b4a8ba
iam4x <iam4x@me.com>
2017-10-26 15:24:39 +0200

    test(breadcrumb): test render & refine()

---
+++ src/__mocks__/base-widget.ts
@@ -0,0 +1,31 @@
+import { Input, OnDestroy, OnInit } from "@angular/core";
+import { noop } from "lodash";
+
+import { NgAisInstance } from "./instantsearch/instantsearch-instance";
+
+export class BaseWidget implements OnInit, OnDestroy {
+  // header footer
+  @Input() public header?: string;
+  @Input() public footer?: string;
+
+  public widget?: Widget;
+  public state?: object;
+
+  constructor(private searchInstance: NgAisInstance) {}
+
+  public createWidget(connector: Connector, options: object = {}) {
+    // nothind to do, test env
+  }
+
+  public ngOnInit() {
+    // nothind to do, test env
+  }
+
+  public ngOnDestroy() {
+    // nothind to do, test env
+  }
+
+  public updateState = (state, isFirstRendering): Promise<void> | void => {
+    this.state = state;
+  };
+}
---
+++ src/breadcrumb/__tests__/breadcrumb.spec.ts
@@ -0,0 +1,61 @@
+import { Component } from "@angular/core";
+import { TestBed } from "@angular/core/testing";
+
+import { NgAisInstantSearchModule } from "../../instantsearch/instantsearch.module";
+import { NgAisBreadcrumbModule } from "../breadcrumb.module";
+import { NgAisBreadcrumb } from "../breadcrumb";
+
+jest.mock("../../base-widget");
+
+describe("Breadcrumb", () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [NgAisInstantSearchModule.forRoot(), NgAisBreadcrumbModule]
+    })
+  );
+
+  it("renders markup without state", () => {
+    const fixture = TestBed.createComponent(NgAisBreadcrumb);
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it("renders markup with two items", () => {
+    const fixture = TestBed.createComponent(NgAisBreadcrumb);
+
+    fixture.componentInstance.updateState(
+      {
+        createURL: jest.fn(),
+        items: [{ name: "foo", value: "foo" }, { name: "bar", value: "bar" }],
+        refine: jest.fn()
+      },
+      false
+    );
+    fixture.detectChanges();
+
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it("should refine when clicking on first item", () => {
+    const fixture = TestBed.createComponent(NgAisBreadcrumb);
+    const spy = jest.fn();
+    fixture.componentInstance.updateState(
+      {
+        createURL: jest.fn(),
+        items: [{ name: "foo", value: "foo" }, { name: "bar", value: "bar" }],
+        refine: spy
+      },
+      false
+    );
+    fixture.detectChanges();
+
+    const firstLink = fixture.debugElement.nativeElement.querySelector(
+      "a:first-child"
+    );
+
+    firstLink.click();
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith("foo");
+  });
+});
