002d68b56a5b3dfdc4fc80e4349c0622a9276621
iam4x <iam4x@me.com>
2017-11-13 22:37:10 +0100

    style(eslint): fix code style errors

--- webpack.demo.js
+++ webpack.demo.js
@@ -1,18 +1,21 @@
-var path = require('path');
-var webpack = require('webpack');
+/* eslint no-param-reassign: 0 */
+/* eslint prefer-rest-params: 0 */
+/* eslint import/no-commonjs: 0 */
 
-var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
 
-var ENV = process.env.MODE;
-var isProd = ENV === 'build';
-var nodeModules = path.join(process.cwd(), 'node_modules');
+const CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const ENV = process.env.MODE;
+const isProd = ENV === 'build';
+const nodeModules = path.join(process.cwd(), 'node_modules');
 
 // Helper functions
 function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
-  return path.join.apply(path, [__dirname].concat(args));
+  return path.join(...[__dirname].concat(args));
 }
 
 module.exports = {
@@ -20,43 +23,43 @@ module.exports = {
 
   entry: {
     polyfills: './examples/demo/polyfill.ts',
-    main: './examples/demo/main.ts'
+    main: './examples/demo/main.ts',
   },
 
   output: {
     path: root('demo', 'dist'),
     publicPath: '/',
     filename: isProd ? 'js/[name].[hash].js' : 'js/[name].js',
-    chunkFilename: isProd ? '[id].[hash].chunk.js' : '[id].chunk.js'
+    chunkFilename: isProd ? '[id].[hash].chunk.js' : '[id].chunk.js',
   },
 
   resolve: {
     modules: [root('demo'), 'node_modules'],
     extensions: ['.ts', '.js', '.html'],
     alias: {
-      'angular-instantsearch': root('src/index.ts')
-    }
+      'angular-instantsearch': root('src/index.ts'),
+    },
   },
 
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: isProd ? '@ngtools/webpack' : 'ts-loader'
+        use: isProd ? '@ngtools/webpack' : 'ts-loader',
       },
       {
         test: /\.ts$/,
-        use: 'angular2-template-loader'
+        use: 'angular2-template-loader',
       },
       {
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        use: 'file-loader?name=fonts/[name].[hash].[ext]?'
+        use: 'file-loader?name=fonts/[name].[hash].[ext]?',
       },
       {
         test: /\.html$/,
-        use: 'raw-loader'
-      }
-    ]
+        use: 'raw-loader',
+      },
+    ],
   },
 
   plugins: [
@@ -64,33 +67,32 @@ module.exports = {
       // Environment helpers
       'process.env': {
         ENV: JSON.stringify(ENV),
-        version: JSON.stringify(require('./package.json').version)
-      }
+        version: JSON.stringify(require('./package.json').version),
+      },
     }),
 
     new CommonsChunkPlugin({
-        "name": "vendor",
-        "minChunks": (module) => module.resource && module.resource.startsWith(nodeModules),
-        "chunks": [
-          "main"
-        ]
+      name: 'vendor',
+      minChunks: module =>
+        module.resource && module.resource.startsWith(nodeModules),
+      chunks: ['main'],
     }),
 
     new CommonsChunkPlugin({
-      names: ['vendor', 'polyfills', 'inline']
+      names: ['vendor', 'polyfills', 'inline'],
     }),
 
     // Inject script and link tags into html files
     // Reference: https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       template: './examples/demo/public/index.html',
-      chunksSortMode: 'dependency'
+      chunksSortMode: 'dependency',
     }),
 
     new webpack.LoaderOptionsPlugin({
       // add debug messages
       debug: !isProd,
-      minimize: isProd
+      minimize: isProd,
     }),
 
     // Workaround to remove Webpack warning in system_js_ng_module_factory_loader.js
@@ -98,12 +100,12 @@ module.exports = {
     new webpack.ContextReplacementPlugin(
       /angular(\\|\/)core(\\|\/)@angular/,
       root('examples', 'demo', 'app')
-    )
+    ),
   ],
 
   devServer: {
     contentBase: 'demo/src/public',
     historyApiFallback: true,
-    stats: 'minimal' // none (or false), errors-only, minimal, normal (or true) and verbose
-  }
-}
+    stats: 'minimal', // none (or false), errors-only, minimal, normal (or true) and verbose
+  },
+};
