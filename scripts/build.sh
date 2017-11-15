17ea04d10330b8b2bbca3ebb3ed1553f4924d60a
iam4x <iam4x@me.com>
2017-11-15 13:58:45 +0100

    chore(build): copy and rename css from instantsearch.css

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,11 +15,21 @@ ngc -p tsconfig-aot.json
 rollup -c rollup.config.umd.js
 rollup -c rollup.config.esm.js
 
+# copy and rename css from instantsearch.css
+cp node_modules/instantsearch.css/themes/*.css dist/bundles
+
+mv dist/bundles/reset.css dist/bundles/instantsearch.css
+mv dist/bundles/reset-min.css dist/bundles/instantsearch.min.css
+
+mv dist/bundles/algolia.css dist/bundles/instantsearch-theme-algolia.css
+mv dist/bundles/algolia-min.css dist/bundles/instantsearch-theme-algolia.min.css
+
 # remove useless files from dist
 (
   cd dist
   rm -rf node_modules src
   find . \
+    \! -name '*.css' \
     \! -name '*.umd.js' \
     \! -name '*.esm.js' \
     \! -name '*.umd.js.map' \
