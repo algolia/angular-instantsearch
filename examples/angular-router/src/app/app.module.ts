32cbe3502c3b0ff4f63553ca6eff277338591c25
iam4x <iam4x@me.com>
2017-11-27 16:36:50 +0100

    feat(examples): add two page SPA

--- examples/angular-router/src/app/app.module.ts
+++ examples/angular-router/src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 import { NgAisModule } from "angular-instantsearch";
 
 import { AppComponent } from "./app.component";
 
+import { IndexComponent } from "./components/index/index.component";
+import { SearchComponent } from "./components/search/search.component";
+
+const appRoutes: Routes = [
+  { path: "search", component: SearchComponent },
+  { path: "", component: IndexComponent }
+];
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, NgAisModule.forRoot()],
+  declarations: [AppComponent, IndexComponent, SearchComponent],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    NgAisModule.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
