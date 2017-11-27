b699b5352680451336d90febb6f209a880b679d5
iam4x <iam4x@me.com>
2017-11-27 18:22:07 +0100

    chore(netlify): build all examples for netlify

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -2,5 +2,15 @@
 
 set -e # exit when error
 
+rm -rf ./netlify-dist/*
+touch ./netlify-dist/.gitkeep
+
+# build examples
 yarn examples:ecommerce:build
+yarn examples:router:build
+
+# build dev-novel
 MODE=build webpack --config webpack.demo.js
+
+mv ./examples/e-commerce/dist ./netlify-dist/e-commerce
+mv ./examples/angular-router/dist ./netlify-dist/angular-router
