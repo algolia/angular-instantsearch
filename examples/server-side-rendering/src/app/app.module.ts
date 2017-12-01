c325f8c8f32b4e50c110663b6ca02b3be7d9524b
iam4x <iam4x@me.com>
2017-12-01 11:59:07 +0100

    docs(examples/ssr): bootstrap simple instantsearch app

--- examples/server-side-rendering/src/app/app.module.ts
+++ examples/server-side-rendering/src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { NgAisModule } from "angular-instantsearch";
 
 import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: "my-app" }),
-    RouterModule.forRoot([
-      { path: "", component: HomeComponent, pathMatch: "full" },
-      { path: "lazy", loadChildren: "./lazy/lazy.module#LazyModule" },
-      { path: "lazy/nested", loadChildren: "./lazy/lazy.module#LazyModule" }
-    ])
+    NgAisModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
