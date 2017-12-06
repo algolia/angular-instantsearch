32d13e5abc8da56b912cba7ebfe75d9f46eba687
iam4x <iam4x@me.com>
2017-12-06 15:53:20 +0100

    docs(examples/ssr): SSR with initial query

--- examples/server-side-rendering/server.ts
+++ examples/server-side-rendering/server.ts
@@ -33,13 +33,16 @@ import { ngExpressEngine } from "@nguniversal/express-engine";
 import { provideModuleMap } from "@nguniversal/module-map-ngfactory-loader";
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine(
-  "html",
-  ngExpressEngine({
+app.engine("html", (_, options, callback) => {
+  const engine = ngExpressEngine({
     bootstrap: AppServerModuleNgFactory,
-    providers: [provideModuleMap(LAZY_MODULE_MAP)]
-  })
-);
+    providers: [
+      { provide: "request", useFactory: () => options.req, deps: [] },
+      provideModuleMap(LAZY_MODULE_MAP)
+    ]
+  });
+  engine(_, options, callback);
+});
 
 app.set("view engine", "html");
 app.set("views", join(DIST_FOLDER, "browser"));
