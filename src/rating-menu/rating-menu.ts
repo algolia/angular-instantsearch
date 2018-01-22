e29c0191da3f2a546a8a2c8c1bae7cf0228bcf53
iam4x <iam4x@me.com>
2018-01-22 14:35:38 +0100

    feat(ratingMenu): update markup to is.css v6

--- src/rating-menu/rating-menu.ts
+++ src/rating-menu/rating-menu.ts
@@ -15,68 +15,63 @@ export type RatingMenuState = {
 @Component({
   selector: "ng-ais-rating-menu",
   template: `
-    <div [class]="cx()">
-      <div
-        [class]="cx('body')"
-        *ngIf="!isHidden"
-      >
-        <svg style="display:none;">
-          <symbol
-            id="ais-StarRating-starSymbol"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-          >
-            <path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/>
-          </symbol>
-          <symbol
-            id="ais-StarRating-starEmptySymbol"
-            viewBox="0 0 24 24"
-            width="24"
-            height="24"
-          >
-            <path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/>
-          </symbol>
-        </svg>
+    <div
+      [class]="cx()"
+      *ngIf="!isHidden"
+    >
+      <svg style="display:none;">
+        <symbol
+          id="ais-StarRating-starSymbol"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/>
+        </symbol>
+        <symbol
+          id="ais-StarRating-starEmptySymbol"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/>
+        </symbol>
+      </svg>
 
-        <ul [class]="cx('list')">
-          <li
-            *ngFor="let item of state.items"
-            [class]="cx('item') + (item.isRefined ? cx('item', 'selected') : '')"
+      <ul [class]="cx('list')">
+        <li
+          *ngFor="let item of state.items"
+          [class]="cx('item') + (item.isRefined ? (' ' + cx('item', 'selected')) : '')"
+          (click)="handleClick($event, item.value)"
+        >
+          <a
+            href="{{state.createURL(item.value)}}"
+            [class]="cx('link')"
+            (click)="handleClick($event, item.value)"
           >
-            <button
-              [class]="cx('button')"
-              [attr.aria-label]="item.name + ' stars & up'"
+            <svg
+              *ngFor="let star of item.stars"
+              [ngClass]="cx('starIcon')"
+              aria-hidden="true"
             >
-              <svg
-                *ngFor="let star of item.stars"
-                [ngClass]="cx('starIcon')"
-                aria-hidden="true"
+              <use
+                *ngIf="star"
+                xlink:href="#ais-StarRating-starSymbol"
               >
-                <use
-                  *ngIf="star"
-                  xlink:href="#ais-StarRating-starSymbol"
-                >
-                </use>
+              </use>
 
-                <use
-                  *ngIf="!star"
-                  xlink:href="#ais-StarRating-starEmptySymbol"
-                >
-                </use>
-              </svg>
+              <use
+                *ngIf="!star"
+                xlink:href="#ais-StarRating-starEmptySymbol"
+              >
+              </use>
+            </svg>
 
-              <span [class]="cx('label')" aria-hidden="true">
-                {{andUpLabel}}
-              </span>
-
-              <span [class]="cx('count')">
-                {{item.count}}
-              </span>
-            </button>
-          </li>
-        </ul>
-      </div>
+            <span [class]="cx('label')" aria-hidden="true">{{andUpLabel}}</span>
+            <span [class]="cx('count')">{{item.count}}</span>
+          </a>
+        </li>
+      </ul>
     </div>
   `
 })
@@ -112,5 +107,14 @@ export class NgAisRatingMenu extends BaseWidget {
       max: this.max
     });
     super.ngOnInit();
+  }
+
+  public handleClick(event: MouseEvent, value: string) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    console.log(value);
+
+    this.state.refine(value);
   }
 }
