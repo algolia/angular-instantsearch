833e86c2146922b26d5fd4302403f8c0993cc0bd
iam4x <iam4x@me.com>
2018-01-22 15:35:57 +0100

    feat(searchBox): update markup to is.css v6

--- src/search-box/search-box.ts
+++ src/search-box/search-box.ts
@@ -16,60 +16,57 @@ import { NgAisInstance } from "../instantsearch/instantsearch-instance";
   selector: "ng-ais-search-box",
   template: `
     <div [class]="cx()">
-      <div [class]="cx('body')">
-        <form
-          [class]="cx('form')"
-          novalidate
-          (submit)="handleSubmit($event)"
+      <form
+        [class]="cx('form')"
+        novalidate
+        (submit)="handleSubmit($event)"
+      >
+        <input
+          [class]="cx('input')"
+          autocapitalize="off"
+          autocorrect="off"
+          placeholder="{{placeholder}}"
+          role="textbox"
+          spellcheck="false"
+          type="text"
+          [value]="state.query"
+          (input)="handleChange($event.target.value)"
+          (focus)="focus.emit($event)"
+          (blur)="blur.emit($event)"
+        />
+
+        <button
+          [class]="cx('submit')"
+          type="submit"
+          title="{{submitTitle}}"
+          (click)="handleSubmit($event)"
         >
-          <input
-            [class]="cx('input')"
-            autocapitalize="off"
-            autocorrect="off"
-            placeholder="{{placeholder}}"
-            role="textbox"
-            spellcheck="false"
-            type="text"
-            [value]="state.query"
-            (input)="handleChange($event.target.value)"
-            (focus)="focus.emit($event)"
-            (blur)="blur.emit($event)"
-          />
-
-          <button
-            [class]="cx('submit')"
-            type="submit"
-            title="{{submitTitle}}"
-            (click)="handleSubmit($event)"
+          <svg
+            [ngClass]="cx('submitIcon')"
+            viewBox="0 0 40 40"
+            width="40"
+            height="40"
           >
-            <svg
-              [ngClass]="cx('submitIcon')"
-              viewBox="0 0 40 40"
-              width="40"
-              height="40"
-            >
-              <path d="M26.804 29.01c-2.832 2.34-6.465 3.746-10.426 3.746C7.333 32.756 0 25.424 0 16.378 0 7.333 7.333 0 16.378 0c9.046 0 16.378 7.333 16.378 16.378 0 3.96-1.406 7.594-3.746 10.426l10.534 10.534c.607.607.61 1.59-.004 2.202-.61.61-1.597.61-2.202.004L26.804 29.01zm-10.426.627c7.323 0 13.26-5.936 13.26-13.26 0-7.32-5.937-13.257-13.26-13.257C9.056 3.12 3.12 9.056 3.12 16.378c0 7.323 5.936 13.26 13.258 13.26z"></path>
-            </svg>
-          </button>
+            <path d="M26.804 29.01c-2.832 2.34-6.465 3.746-10.426 3.746C7.333 32.756 0 25.424 0 16.378 0 7.333 7.333 0 16.378 0c9.046 0 16.378 7.333 16.378 16.378 0 3.96-1.406 7.594-3.746 10.426l10.534 10.534c.607.607.61 1.59-.004 2.202-.61.61-1.597.61-2.202.004L26.804 29.01zm-10.426.627c7.323 0 13.26-5.936 13.26-13.26 0-7.32-5.937-13.257-13.26-13.257C9.056 3.12 3.12 9.056 3.12 16.378c0 7.323 5.936 13.26 13.258 13.26z"></path>
+          </svg>
+        </button>
 
-          <button
-            [class]="cx('reset')"
-            type="reset"
-            title="{{resetTitle}}"
-            (click)="handleReset($event)"
-            style="display: none"
+        <button
+          [class]="cx('reset')"
+          type="reset"
+          title="{{resetTitle}}"
+          (click)="handleReset($event)"
+          [hidden]="!state.query || (state.query && !state.query.trim())">
+          <svg
+            [ngClass]="cx('resetIcon')"
+            viewBox="0 0 20 20"
+            width="20"
+            height="20"
           >
-            <svg
-              [ngClass]="cx('resetIcon')"
-              viewBox="0 0 20 20"
-              width="20"
-              height="20"
-            >
-              <path d="M8.114 10L.944 2.83 0 1.885 1.886 0l.943.943L10 8.113l7.17-7.17.944-.943L20 1.886l-.943.943-7.17 7.17 7.17 7.17.943.944L18.114 20l-.943-.943-7.17-7.17-7.17 7.17-.944.943L0 18.114l.943-.943L8.113 10z"></path>
-            </svg>
-          </button>
-        </form>
-      </div>
+            <path d="M8.114 10L.944 2.83 0 1.885 1.886 0l.943.943L10 8.113l7.17-7.17.944-.943L20 1.886l-.943.943-7.17 7.17 7.17 7.17.943.944L18.114 20l-.943-.943-7.17-7.17-7.17 7.17-.944.943L0 18.114l.943-.943L8.113 10z"></path>
+          </svg>
+        </button>
+      </form>
     </div>
   `
 })
