d33aff83c67d6f5c19d4f70e35de9555d74c898f
iam4x <iam4x@me.com>
2018-01-30 18:34:59 +0100

    chore(examples): use new API

--- examples/dev-novel/custom-widgets.ts
+++ examples/dev-novel/custom-widgets.ts
@@ -1,5 +1,5 @@
-import { Component, Inject, PLATFORM_ID } from "@angular/core";
-import { BaseWidget, NgAisInstance } from "angular-instantsearch";
+import { Component, Inject, forwardRef } from "@angular/core";
+import { BaseWidget, NgAisInstantSearch } from "angular-instantsearch";
 import { connectMenu } from "instantsearch.js/es/connectors";
 
 @Component({
@@ -21,10 +21,10 @@ import { connectMenu } from "instantsearch.js/es/connectors";
 })
 export class MenuSelect extends BaseWidget {
   constructor(
-    @Inject(PLATFORM_ID) public platformId: Object,
-    searchInstance: NgAisInstance
+    @Inject(forwardRef(() => NgAisInstantSearch))
+    public instantSearchParent
   ) {
-    super(searchInstance, "MenuSelect");
+    super("MenuSelect");
   }
 
   public ngOnInit() {
