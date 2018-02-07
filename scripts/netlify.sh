b444bfa390d81adee94d369db58d7335d3269b00
iam4x <iam4x@me.com>
2018-02-07 15:48:16 -0800

    chore(scripts/netlify): build media example

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -8,9 +8,11 @@ touch ./netlify-dist/.gitkeep
 # build examples
 yarn examples:ecommerce:build
 yarn examples:router:build
+yarn examples:media:build
 
 # build dev-novel
 MODE=build webpack --config webpack.demo.js
 
 mv ./examples/e-commerce/dist ./netlify-dist/e-commerce
 mv ./examples/angular-router/dist ./netlify-dist/angular-router
+mv ./examples/media/dist ./netlify-dist/media
