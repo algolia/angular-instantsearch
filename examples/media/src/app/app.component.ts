508def55c4b2deefb2ad4a17f3f92099b8e172b2
iam4x <iam4x@me.com>
2018-02-07 15:36:26 -0800

    docs(examples/media): add rating

--- examples/media/src/app/app.component.ts
+++ examples/media/src/app/app.component.ts
@@ -4,4 +4,15 @@ import { Component } from "@angular/core";
   selector: "app-root",
   templateUrl: "./app.component.html"
 })
-export class AppComponent {}
+export class AppComponent {
+  transformHits(hits) {
+    hits.forEach(hit => {
+      hit.stars = [];
+      for (let i = 1; i <= 5; i) {
+        hit.stars.push(i <= hit.rating);
+        i += 1;
+      }
+    });
+    return hits;
+  }
+}
