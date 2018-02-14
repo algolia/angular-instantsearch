fdff7a6fb65676cd85714e369ff6999df1725a42
iam4x <iam4x@me.com>
2018-02-14 15:27:27 -0800

    docs(community): update `editThisPage` code

--- community-website/src/community-project-boilerplate-docgen/assets/js/editThisPage.js
+++ community-website/src/community-project-boilerplate-docgen/assets/js/editThisPage.js
@@ -3,22 +3,15 @@ if (document.querySelector('.documentation-container')) {
   $edit.classList.add('editThisPage');
   $edit.textContent = 'Edit this page';
 
-  let href = 'https://github.com/algolia/react-instantsearch/edit/master/';
-  const doc = 'docgen/src';
-  const api = 'packages/react-instantsearch/src';
+  let href = 'https://github.com/algolia/angular-instantsearch/edit/master/';
+  const doc = 'community-website/src/community-project-boilerplate-docgen/src';
 
-  let pathname = document.location.pathname.replace('/react-instantsearch', '');
+  let pathname = document.location.pathname.replace(
+    '/angular-instantsearch',
+    ''
+  );
 
-  if (/^\/(?:widgets|connectors)\/.+/.test(pathname)) {
-    href += `${api}${pathname.replace('.html', '.js')}`;
-    if (pathname === '/widgets/InstantSearch.html') {
-      href = href.replace('/widgets/', '/core/');
-    }
-  } else {
-    if (/\/$/.test(pathname)) pathname += 'index.html';
-    href += `${doc}${pathname.replace('.html', '.md')}`;
-  }
-
+  href += `${doc}${pathname.replace('.html', '.md')}`;
   pathname = pathname.replace('.html', '.md');
   $edit.href = href;
   document.querySelector('.documentation-container').appendChild($edit);
