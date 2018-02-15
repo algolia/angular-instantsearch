83b1461bada9280fd54ec735f793e6fdc1cdda74
iam4x <iam4x@me.com>
2018-02-15 14:46:44 -0800

    fix(netlify): install node_modules before building doc

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -5,6 +5,12 @@ set -e # exit when error
 rm -rf ./netlify-dist/*
 touch ./netlify-dist/.gitkeep
 
+# build community website
+(cd community-website && yarn)
+(cd community-website/src/community-project-boilerplate-docgen && yarn)
+yarn doc:build
+mv ./community-website/docs/* ./netlify-dist/
+
 # build examples
 yarn examples:ecommerce:build
 yarn examples:router:build
@@ -12,10 +18,6 @@ yarn examples:media:build
 
 # build dev-novel
 MODE=build webpack --config webpack.demo.js
-
-# build community website
-yarn doc:build
-mv ./community-website/docs/* ./netlify-dist/
 
 mv ./examples/e-commerce/dist ./netlify-dist/examples/e-commerce
 mv ./examples/angular-router/dist ./netlify-dist/examples/angular-router
