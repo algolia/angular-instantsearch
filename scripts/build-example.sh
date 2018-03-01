e2f2c5a2c2364c6f50975f346a31f1207c501ee9
vvo <vincent.voyer@algolia.com>
2018-03-01 08:47:43 +0100

    chore(netlify): fix build

--- scripts/build-example.sh
+++ scripts/build-example.sh
@@ -8,7 +8,7 @@ yarn build
   cd examples/$1
   rm -rf node_modules
   yarn
-  mkdir ./node_modules/angular-instantsearch
+  mkdir -p ./node_modules/angular-instantsearch
   cp -R ../../dist/* ./node_modules/angular-instantsearch
   ./node_modules/.bin/ng build --prod --base-href "." -d ""
 )
