1c34e6bf33aaeee5cd494910bcc9672b4ec2872d
François Chalifour <francois.chalifour@gmail.com>
2018-05-03 16:57:42 +0200

    feat: Add `createSSRSearchClient` as a `createSSRAlgoliaClient` alternative

--- src/create-ssr-algolia-client.ts
+++ src/create-ssr-algolia-client.ts
@@ -13,64 +13,85 @@ export function createSSRAlgoliaClient({
   httpClient,
   HttpHeaders,
   transferState,
+  makeStateKey,
+}) {
+  console.warn(
+    '`createSSRAlgoliaClient` is deprecated in favor of `createSSRSearchClient` to be plugged to `searchClient`.'
+  );
+
+  return (_, appId, apiKey) =>
+    createSSRSearchClient({
+      appId,
+      apiKey,
+      httpClient,
+      HttpHeaders,
+      transferState,
+      makeStateKey,
+    });
+}
+
+export function createSSRSearchClient({
+  appId,
+  apiKey,
+  httpClient,
+  HttpHeaders,
+  transferState,
   makeStateKey
 }) {
-  return (_, appId, apiKey) => {
-    const client = algoliasearch(appId, apiKey, {});
-    client.addAlgoliaAgent(`angular-instantsearch ${VERSION}`);
+  const client = algoliasearch(appId, apiKey, {});
+  client.addAlgoliaAgent(`angular-instantsearch ${VERSION}`);
 
-    client._request = (rawUrl, opts) => {
-      let headers = new HttpHeaders();
+  client._request = (rawUrl, opts) => {
+    let headers = new HttpHeaders();
 
-      headers = headers.set(
-        "content-type",
-        opts.method === "POST"
-          ? "application/x-www-form-urlencoded"
-          : "application/json"
-      );
+    headers = headers.set(
+      "content-type",
+      opts.method === "POST"
+        ? "application/x-www-form-urlencoded"
+        : "application/json"
+    );
 
-      headers = headers.set("accept", "application/json");
+    headers = headers.set("accept", "application/json");
 
-      const url =
-        rawUrl + (rawUrl.includes("?") ? "&" : "?") + encode(opts.headers);
+    const url =
+      rawUrl + (rawUrl.includes("?") ? "&" : "?") + encode(opts.headers);
 
-      const transferStateKey = makeStateKey(`ngais(${opts.body})`);
+    const transferStateKey = makeStateKey(`ngais(${opts.body})`);
 
-      if (transferState.hasKey(transferStateKey)) {
-        const resp = JSON.parse(transferState.get(transferStateKey, {}));
-        return Promise.resolve({
-          statusCode: resp.status,
-          body: resp.body,
-          headers: resp.headers
-        });
-      }
-
-      return new Promise((resolve, reject) => {
-        httpClient
-          .request(opts.method, url, {
-            headers,
-            body: opts.body,
-            observe: "response"
-          })
-          .subscribe(
-            resp => {
-              transferState.set(transferStateKey, JSON.stringify(resp));
-              resolve({
-                statusCode: resp.status,
-                body: resp.body,
-                headers: resp.headers
-              });
-            },
-            resp =>
-              reject({
-                statusCode: resp.status,
-                body: resp.body,
-                headers: resp.headers
-              })
-          );
+    if (transferState.hasKey(transferStateKey)) {
+      const resp = JSON.parse(transferState.get(transferStateKey, {}));
+      return Promise.resolve({
+        statusCode: resp.status,
+        body: resp.body,
+        headers: resp.headers
       });
-    };
+    }
 
-    return client;
+    return new Promise((resolve, reject) => {
+      httpClient
+        .request(opts.method, url, {
+          headers,
+          body: opts.body,
+          observe: "response"
+        })
+        .subscribe(
+          resp => {
+            transferState.set(transferStateKey, JSON.stringify(resp));
+            resolve({
+              statusCode: resp.status,
+              body: resp.body,
+              headers: resp.headers
+            });
+          },
+          resp =>
+            reject({
+              statusCode: resp.status,
+              body: resp.body,
+              headers: resp.headers
+            })
+        );
+    });
   };
+
+  return client;
 }
