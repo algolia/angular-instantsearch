b7dd529d5b72953ba0bf3b29f0f4dac677bce736
iam4x <iam4x@me.com>
2018-05-23 11:23:01 +0200

    docs(examples/media): upgrade to v6

--- examples/media/src/polyfills.ts
+++ examples/media/src/polyfills.ts
@@ -65,3 +65,4 @@ import "zone.js/dist/zone"; // Included with Angular CLI.
  * Need to import at least one locale-data with intl.
  */
 // import 'intl/locale-data/jsonp/en';
+(window as any).process = { env: {} };
