8200ac1de7c8866a94f63834299799a27ff996ef
iam4x <iam4x@me.com>
2018-05-23 11:11:59 +0200

    fix(run-examples): copy instantsearch.js as well

--- scripts/run-example.sh
+++ scripts/run-example.sh
@@ -6,8 +6,17 @@ yarn build
 
 (
   cd examples/$1
+  yarn
+
+  # copy angular-instantsearch
   rm -rf ./node_modules/angular-instantsearch
   mkdir -p ./node_modules/angular-instantsearch
   cp -R ../../dist/* ./node_modules/angular-instantsearch
+
+  # copy same instantsearch.js version
+  rm -rf ./node_modules/instantsearch.js
+  mkdir -p ./node_modules/instantsearch.js
+  cp -R ../../node_modules/instantsearch.js/* ./node_modules/instantsearch.js
+
   yarn start
 )
