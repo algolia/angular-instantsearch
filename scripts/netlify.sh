bbaabbf035dd9821514af143eb91b6c4d27f4932
Alex S <bobylito@users.noreply.github.com>
2018-09-12 14:33:29 +0200

    docs: add back e-commerce example (#248)
    * revert: docs: remove e-commerce demo
    
    This reverts commit 1427c45c0b792b95caac9fad8407f7a4aa10c743.
    
    * revert: docs: remove e-commerce demo
    
    This reverts commit 3c36e536d1f6a856ca95c453fc0945d06c66a44f.
    
    * docs: update e-commerce index
    
    * docs(examples): fix broken layout on large screen

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -10,11 +10,13 @@ mkdir -p ./netlify-dist/examples
 mv ./community-website/docs/* ./netlify-dist/
 
 # build examples
+yarn examples:ecommerce:build
 yarn examples:router:build
 yarn examples:media:build
 
 # build dev-novel
 MODE=build webpack --config webpack.demo.js
 
+mv ./examples/e-commerce/dist ./netlify-dist/examples/e-commerce
 mv ./examples/angular-router/dist ./netlify-dist/examples/angular-router
 mv ./examples/media/dist ./netlify-dist/examples/media
