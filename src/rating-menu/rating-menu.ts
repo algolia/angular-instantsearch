beb2610d5fbd4beb52c12c95d4ee501fcc32fef1
Youcef Mammar <youcef.mammar@algolia.com>
2019-02-10 13:22:53 +0100

    migrate rating-menu

--- src/rating-menu/rating-menu.ts
+++ src/rating-menu/rating-menu.ts
@@ -1,14 +1,22 @@
 import { Component, Input, Inject, forwardRef } from '@angular/core';
 
-import { connectStarRating } from 'instantsearch.js/es/connectors';
+import { connectRatingMenu } from 'instantsearch.js/es/connectors';
 import { BaseWidget } from '../base-widget';
 import { NgAisInstantSearch } from '../instantsearch/instantsearch';
 import { noop } from '../utils';
 
+export type RatingMenuItem = {
+  count: number;
+  isRefined: boolean;
+  name: string;
+  value: string;
+  stars: boolean[];
+};
+
 export type RatingMenuState = {
   createURL: Function;
   hasNoResults: boolean;
-  items: {}[];
+  items: RatingMenuItem[];
   refine: Function;
 };
 
@@ -81,7 +89,7 @@ export class NgAisRatingMenu extends BaseWidget {
 
   // connectors options
   @Input() public attribute: string;
-  @Input() public max?: number = 5;
+  @Input() public max?: number;
 
   public state: RatingMenuState = {
     createURL: noop,
@@ -102,8 +110,8 @@ export class NgAisRatingMenu extends BaseWidget {
   }
 
   public ngOnInit() {
-    this.createWidget(connectStarRating, {
-      attributeName: this.attribute,
+    this.createWidget(connectRatingMenu, {
+      attribute: this.attribute,
       max: this.max,
     });
     super.ngOnInit();
