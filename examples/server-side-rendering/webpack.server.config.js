d54b068c0672dc6b815b98cb0893265b26cd811d
Youcef Mammar <youcef.mammar@algolia.com>
2019-02-22 13:37:13 +0100

    fix restore externals in webpack.config

--- examples/server-side-rendering/webpack.server.config.js
+++ examples/server-side-rendering/webpack.server.config.js
@@ -14,7 +14,7 @@ module.exports = {
   target: 'node',
   resolve: { extensions: ['.ts', '.js'] },
   // Make sure we include all node_modules etc
-  externals: [/node_modules/],
+  externals: [/(node_modules|main\..*\.js)/,],
   output: {
     // Puts the output at the root of the dist folder
     path: path.join(__dirname, 'dist'),
