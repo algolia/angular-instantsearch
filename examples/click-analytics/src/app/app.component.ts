9811332aad402250eaccdc7a679872f28db8e2ae
Youcef Mammar <youcef.mammar@algolia.com>
2019-02-27 14:32:34 +0100

    POC: implement clickAnalytics

--- examples/click-analytics/src/app/app.component.ts
+++ examples/click-analytics/src/app/app.component.ts
@@ -9,5 +9,7 @@ export class AppComponent {
     appId: 'latency',
     apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
     indexName: 'instant_search',
+    // @ts-ignore
+    insightsClient: window.aa,
   };
 }
