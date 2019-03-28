047183f112c2049131fa3ff14d6fec685a8cafbd
Robert Mogos <robert.mogos@algolia.com>
2019-03-28 17:05:05 +0100

    fix(ua): Add test when client doesn't implement addAlgoliaAgent

--- src/instantsearch/__tests__/instantsearch.spec.ts
+++ src/instantsearch/__tests__/instantsearch.spec.ts
@@ -4,14 +4,14 @@ import { VERSION } from '../../version';
 import { TestBed } from '@angular/core/testing';
 jest.mock('instantsearch.js/es', () => ({
   default: () => {
-    return { on: jest.fn(), start: jest.fn() };
+    return { on: jest.fn(), start: jest.fn(), removeListener: jest.fn(), dispose: jest.fn() };
   },
 }));
 
 jest.mock('../../../src/base-widget');
 
 describe('InstantSearch', () => {
-  it('should add user agent stuff when the client is provided to the config', () => {
+  it('should add user agent when the client is provided to the config', () => {
     const addAlgoliaAgent = jest.fn();
     @Component({
       template: `<ais-instantsearch [config]="config"> </ais-instantsearch>`,
@@ -42,5 +42,31 @@ describe('InstantSearch', () => {
     expect(addAlgoliaAgent).toHaveBeenCalledWith(
       `angular-instantsearch (${VERSION})`
     );
+  });
+
+  it('should not add a user agent when the client is not provided to the config', () => {
+    @Component({
+      template: `<ais-instantsearch [config]="config"> </ais-instantsearch>`,
+    })
+    class TestContainer {
+      public config = {
+        appId: 'theAppId',
+        apiKey: 'theApiKey',
+        indexName: 'theIndexName',
+        searchClient: {},
+      };
+    }
+
+    TestBed.configureCompiler({ preserveWhitespaces: true } as any)
+      .configureTestingModule({
+        imports: [NgAisInstantSearchModule.forRoot()],
+        declarations: [TestContainer],
+      })
+      .compileComponents();
+
+    expect(() => {
+      const fixture = TestBed.createComponent(TestContainer);
+      fixture.detectChanges();
+    }).not.toThrowError();
   });
 });
