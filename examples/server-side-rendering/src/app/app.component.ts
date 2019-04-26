7594cfac68891658e139936126f05a7cb3225d3c
Youcef Mammar <tkrugg@users.noreply.github.com>
2019-04-26 15:26:45 +0200

    chore(examples): migrate ssr example to v3 (#497)

--- examples/server-side-rendering/src/app/app.component.ts
+++ examples/server-side-rendering/src/app/app.component.ts
@@ -43,9 +43,9 @@ import {
                 header="Sort by"
                 [items]="
                   [
-                    {name: 'instant_search', label: 'Featured'},
-                    {name: 'instant_search_price_asc', label: 'Price asc.'},
-                    {name: 'instant_search_price_desc', label: 'Price desc.'}
+                    {value: 'instant_search', label: 'Featured'},
+                    {value: 'instant_search_price_asc', label: 'Price asc.'},
+                    {value: 'instant_search_price_desc', label: 'Price desc.'}
                   ]
                 "
               >
@@ -79,7 +79,7 @@ export class AppComponent {
     this.instantsearchConfig = {
       searchParameters,
       indexName: 'instant_search',
-      urlSync: true,
+      routing: true,
       searchClient: createSSRSearchClient({
         makeStateKey,
         HttpHeaders,
