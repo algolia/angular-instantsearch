08cc988577341722c29daafa33a44272ff1393fd
Youcef Mammar <youcef.mammar@algolia.com>
2019-05-16 16:30:46 -0400

    fix(SortBy): use connector transformItems
    **Summary**
    This migrates SortBy to use the transformItems provided by the connector. It also removes the test related and replaces it with one that checks all instance options are correctly applied to the connector.

--- src/sort-by/__tests__/sort-by.spec.ts
+++ src/sort-by/__tests__/sort-by.spec.ts
@@ -1,3 +1,4 @@
+import { connectSortBy } from 'instantsearch.js/es/connectors';
 import { createRenderer } from '../../../helpers/test-renderer';
 import { NgAisSortBy } from '../sort-by';
 
@@ -7,24 +8,33 @@ const defaultState = {
   refine: jest.fn(),
 };
 
-const render = createRenderer({
-  defaultState,
-  template: '<ais-sort-by></ais-sort-by>',
-  TestedWidget: NgAisSortBy,
-});
-
 describe('SortBy', () => {
   it('renders markup without state', () => {
+    const render = createRenderer({
+      defaultState,
+      template: '<ais-sort-by></ais-sort-by>',
+      TestedWidget: NgAisSortBy,
+    });
     const fixture = render();
     expect(fixture).toMatchSnapshot();
   });
 
   it('renders markup with state', () => {
+    const render = createRenderer({
+      defaultState,
+      template: '<ais-sort-by></ais-sort-by>',
+      TestedWidget: NgAisSortBy,
+    });
     const fixture = render({});
     expect(fixture).toMatchSnapshot();
   });
 
   it('should call refine on select change', () => {
+    const render = createRenderer({
+      defaultState,
+      template: '<ais-sort-by></ais-sort-by>',
+      TestedWidget: NgAisSortBy,
+    });
     const refine = jest.fn();
     const fixture = render({ refine });
 
@@ -34,5 +44,30 @@ describe('SortBy', () => {
 
     expect(refine).toHaveBeenCalled();
     expect(refine).toHaveBeenCalledWith('bar');
+  });
+
+  it('should create widget with connectSortBy and pass instance options', () => {
+    const createWidget = jest.spyOn(NgAisSortBy.prototype, 'createWidget');
+    const items = [
+      { label: 'foo', value: 'foo' },
+      { label: 'bar', value: 'bar' },
+    ];
+    const transformItems = jest.fn(x => x);
+    const render = createRenderer({
+      defaultState,
+      template:
+        '<ais-sort-by [items]="items" [transformItems]="transformItems"></ais-sort-by>',
+      TestedWidget: NgAisSortBy,
+      methods: { items, transformItems },
+    });
+
+    render();
+
+    expect(createWidget).toHaveBeenCalledTimes(1);
+    expect(createWidget).toHaveBeenCalledWith(connectSortBy, {
+      items,
+      transformItems,
+    });
+    createWidget.mockRestore();
   });
 });
