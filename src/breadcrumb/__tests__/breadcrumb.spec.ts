735c2a118ffb037129fa9566fabb80c0105f1ca5
Youcef Mammar <tkrugg@users.noreply.github.com>
2019-05-20 22:17:21 -0700

    fix(Breadcrumb): add missing instance options and types (#515)
    * fix(Breadcrumb): add missing instance options
    
    This adds missing instance options to Breadcrumbs, namely
    - `separator`
    - `transformItems`
    
    Full list of instance options is here: https://www.algolia.com/doc/api-reference/widgets/breadcrumb/js/?language=angular#connector-param-init-instance-options
    
    * fix(Breadcrumb): add missing `transformItems` story
    
    I would have added a story `separator` as well but my understanding is
    implies using another dataset - not sure it's worth the trouble since
    the separator is mostly just passed to the connector.
    
    * fix(Breadcrumbs): add type to refine and createURL
    
    * fix(Breadcrumbs): change createURL default to () => '#'

--- src/breadcrumb/__tests__/breadcrumb.spec.ts
+++ src/breadcrumb/__tests__/breadcrumb.spec.ts
@@ -1,3 +1,4 @@
+import { connectBreadcrumb } from 'instantsearch.js/es/connectors';
 import { createRenderer } from '../../../helpers/test-renderer';
 import { NgAisBreadcrumb } from '../breadcrumb';
 
@@ -7,24 +8,33 @@ const defaultState = {
   refine: jest.fn(),
 };
 
-const render = createRenderer({
-  defaultState,
-  template: '<ais-breadcrumb></ais-breadcrumb>',
-  TestedWidget: NgAisBreadcrumb,
-});
-
 describe('Breadcrumb', () => {
   it('renders markup without state', () => {
+    const render = createRenderer({
+      defaultState,
+      template: '<ais-breadcrumb></ais-breadcrumb>',
+      TestedWidget: NgAisBreadcrumb,
+    });
     const fixture = render();
     expect(fixture).toMatchSnapshot();
   });
 
   it('renders markup with two items', () => {
+    const render = createRenderer({
+      defaultState,
+      template: '<ais-breadcrumb></ais-breadcrumb>',
+      TestedWidget: NgAisBreadcrumb,
+    });
     const fixture = render({});
     expect(fixture).toMatchSnapshot();
   });
 
   it('should refine when clicking on first item', () => {
+    const render = createRenderer({
+      defaultState,
+      template: '<ais-breadcrumb></ais-breadcrumb>',
+      TestedWidget: NgAisBreadcrumb,
+    });
     const refine = jest.fn();
     const fixture = render({ refine });
 
@@ -39,10 +49,46 @@ describe('Breadcrumb', () => {
   });
 
   it('should be hidden with `autoHideContainer`', () => {
+    const render = createRenderer({
+      defaultState,
+      template: '<ais-breadcrumb></ais-breadcrumb>',
+      TestedWidget: NgAisBreadcrumb,
+    });
     const fixture = render({ items: [] });
     fixture.componentInstance.testedWidget.autoHideContainer = true;
     fixture.detectChanges();
 
     expect(fixture).toMatchSnapshot();
+  });
+
+  it('should create widget with connectBreadcrumb and pass instance options', () => {
+    const createWidget = jest.spyOn(NgAisBreadcrumb.prototype, 'createWidget');
+
+    const attributes = ['attr.lvl1', 'attr.lvl2'];
+    const transformItems = jest.fn(x => x);
+
+    const render = createRenderer({
+      defaultState,
+      template: `
+        <ais-breadcrumb 
+          [attributes]="attributes"
+          rootPath="Audio" 
+          separator=" / " 
+          [transformItems]="transformItems">
+        </ais-breadcrumb>`,
+      TestedWidget: NgAisBreadcrumb,
+      methods: { transformItems, attributes },
+    });
+
+    render({});
+
+    expect(createWidget).toHaveBeenCalledWith(connectBreadcrumb, {
+      attributes: ['attr.lvl1', 'attr.lvl2'],
+      rootPath: 'Audio',
+      separator: ' / ',
+      transformItems,
+    });
+
+    createWidget.mockRestore();
   });
 });
