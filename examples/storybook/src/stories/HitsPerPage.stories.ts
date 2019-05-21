05b4f9603001baa455d6e8e828a72a9189f1ccf6
Youcef Mammar <tkrugg@users.noreply.github.com>
2019-05-21 16:12:29 -0700

    fix(HitsPerPage): use connector transformItems (#562)
    * fix(HitsPerPage): use connector transformItems
    
    This migrates HitsPerPage to use the transformItems provided by the connector.
    
    ref #372
    
    * docs(HitsPerPage): add storybook example to showcase transformItems
    
    * fix: separate instance and rendering items types

--- examples/storybook/src/stories/HitsPerPage.stories.ts
+++ examples/storybook/src/stories/HitsPerPage.stories.ts
@@ -31,4 +31,24 @@ storiesOf('HitsPerPage', module)
         </ais-hits-per-page>
       `,
     }),
-  }));
+  }))
+  .add('with transformItems', () => {
+    const transformItems = items =>
+      items.map(item => ({ ...item, label: `${item.label} (transformed)` }));
+    return {
+      component: wrapWithHits({
+        template: `
+        <ais-hits-per-page
+          [items]="[
+            { value: 3, label: '3 per page' },
+            { value: 5, label: '5 per page', default: true },
+            { value: 10, label: '10 per page' }
+          ]"
+          [transformItems]="transformItems"
+        >
+        </ais-hits-per-page>
+        `,
+        methods: { transformItems },
+      }),
+    };
+  });
