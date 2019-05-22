a52442af387a2e038fb03ada0f20edba7966aa9c
Eunjae Lee <eunjae.lee@algolia.com>
2019-05-22 16:48:58 +0200

    chore(storybook): pass styles prop to wrapped component (#582)

--- examples/storybook/src/wrap-with-hits.ts
+++ examples/storybook/src/wrap-with-hits.ts
@@ -8,7 +8,7 @@ type Helper = {
 
 type WrapWithHitsParams = {
   template: string;
-  styles?: string;
+  styles?: string[];
   searchParameters?: {};
   methods?: {};
   searchFunction?: (helper: Helper) => void;
@@ -48,7 +48,7 @@ const defaultHits = `
 
 export function wrapWithHits({
   template,
-  styles = '',
+  styles = [],
   searchParameters = {},
   methods = {},
   searchFunction,
@@ -62,6 +62,7 @@ export function wrapWithHits({
 }: WrapWithHitsParams) {
   @Component({
     selector: 'ais-app',
+    styles,
     template: `
       <ais-instantsearch [config]="config">
         <div class="ais-container ais-container-preview">
