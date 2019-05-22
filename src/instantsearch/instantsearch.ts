3526015cae8a02f4b1930762838275825ff4e71b
Youcef Mammar <tkrugg@users.noreply.github.com>
2019-05-22 16:36:35 -0700

    fix(types): update Hit type (#579)
    **Summary**
    This type spec is copy pasted from the one we currently have in InstantSearch.
    
    https://github.com/algolia/instantsearch.js/blob/65911504b9ae0e2596c75f0ae74133985dc79850/src/types/instantsearch.ts#L20-L71
    
    Generally copy pasting is not a good idea but [this comment](https://github.com/algolia/angular-instantsearch/pull/553/files#r284135258) makes the point that since it's mostely Search API spec, it may not change much.
    
    If this is merged, both of this PR need to be updated
    - fix(Hits): use connector transformItems -> https://github.com/algolia/angular-instantsearch/pull/551
    - fix(InfiniteHits): improve typing -> https://github.com/algolia/angular-instantsearch/pull/553
    
    **Results**
    N/A

--- src/instantsearch/instantsearch.ts
+++ src/instantsearch/instantsearch.ts
@@ -135,8 +135,57 @@ export type SearchResponse = {
   index?: string;
 };
 
+type HitAttributeHighlightResult = {
+  value: string;
+  matchLevel: 'none' | 'partial' | 'full';
+  matchedWords: string[];
+  fullyHighlighted?: boolean;
+};
+
+type HitHighlightResult = {
+  [attribute: string]:
+    | HitAttributeHighlightResult
+    | HitAttributeHighlightResult[]
+    | HitHighlightResult;
+};
+
+type HitAttributeSnippetResult = Pick<
+  HitAttributeHighlightResult,
+  'value' | 'matchLevel'
+>;
+
+type HitSnippetResult = {
+  [attribute: string]:
+    | HitAttributeSnippetResult
+    | HitAttributeSnippetResult[]
+    | HitSnippetResult;
+};
+
 export type Hit = {
-  _highlightResult?: object;
+  [attribute: string]: any;
+  objectID: string;
+  _highlightResult?: HitHighlightResult;
+  _snippetResult?: HitSnippetResult;
+  _rankingInfo?: {
+    promoted: boolean;
+    nbTypos: number;
+    firstMatchedWord: number;
+    proximityDistance?: number;
+    geoDistance: number;
+    geoPrecision?: number;
+    nbExactWords: number;
+    words: number;
+    filters: number;
+    userScore: number;
+    matchedGeoLocation?: {
+      lat: number;
+      lng: number;
+      distance: number;
+    };
+  };
+  _distinctSeqID?: number;
+  __position: number;
+  __queryID?: string;
 };
 
 // Documentation: https://www.algolia.com/doc/rest-api/search/?language=javascript#search-for-facet-values
