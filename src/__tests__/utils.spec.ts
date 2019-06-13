497d7142751a08f75a87ed5d7e7d77b8e09c56ca
Eunjae Lee <eunjae.lee@algolia.com>
2019-06-13 14:39:34 +0200

    test(utils): add tests for bem (#614)
    * test(utils): add tests for bem
    
    * chore: remove special class names for header, body, footer
    
    BREAKING CHANGE: `ais-body` class has been removed from `ais-range-slide` component
    
    * test(utils): add test for root element

---
+++ src/__tests__/utils.spec.ts
@@ -0,0 +1,20 @@
+import { bem } from '../utils';
+
+describe('Utils', () => {
+  const cx = bem('testWidget');
+
+  describe('bem', () => {
+    it('generates className for root', () => {
+      expect(cx()).toBe('ais-testWidget');
+      expect(cx('')).toBe('ais-testWidget');
+    });
+
+    it('generates className for an element', () => {
+      expect(cx('input')).toBe('ais-testWidget-input');
+    });
+
+    it('generates className for subElement', () => {
+      expect(cx('input', 'min')).toBe('ais-testWidget-input--min');
+    });
+  });
+});
