adc88a3afd62b530f2bbfb502eb8766840c7a050
Eunjae Lee <eunjae.lee@algolia.com>
2019-06-14 18:09:33 +0200

    chore(examples): remove log from example builds

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -15,10 +15,10 @@ yarn
 yarn build
 
 # build examples
-SKIP_PACKAGE_BUILD=true yarn examples:ecommerce:build
-SKIP_PACKAGE_BUILD=true yarn examples:router:build
-SKIP_PACKAGE_BUILD=true yarn examples:media:build
-SKIP_PACKAGE_BUILD=true yarn examples:storybook:build
+SKIP_PACKAGE_BUILD=true yarn examples:ecommerce:build &> /dev/null
+SKIP_PACKAGE_BUILD=true yarn examples:router:build &> /dev/null
+SKIP_PACKAGE_BUILD=true yarn examples:media:build &> /dev/null
+SKIP_PACKAGE_BUILD=true yarn examples:storybook:build &> /dev/null
 
 mv ./examples/e-commerce/dist ./netlify-dist/examples/e-commerce
 mv ./examples/angular-router/dist ./netlify-dist/examples/angular-router
