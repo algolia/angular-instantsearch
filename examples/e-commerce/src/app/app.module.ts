ed57b31ae824f7cee6b8cbbc53408bbf2eb95527
Eunjae Lee <eunjae.lee@algolia.com>
2019-06-14 16:29:48 +0200

    chore(examples): remove unnecessary components

--- examples/e-commerce/src/app/app.module.ts
+++ examples/e-commerce/src/app/app.module.ts
@@ -3,19 +3,11 @@ import { NgModule } from '@angular/core';
 import { NgAisModule } from 'angular-instantsearch';
 
 import { AppComponent } from './app.component';
-import { ProductImageComponent } from './product-image/product-image.component';
-import { PriceSlider } from './price-slider/price-slider.component';
 import { RatingMenu } from './rating-menu/rating-menu.component';
 import { Snippet } from './snippet/snippet.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductImageComponent,
-    PriceSlider,
-    RatingMenu,
-    Snippet,
-  ],
+  declarations: [AppComponent, RatingMenu, Snippet],
   imports: [BrowserModule, NgAisModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
