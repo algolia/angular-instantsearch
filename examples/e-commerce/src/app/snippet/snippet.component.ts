fe05cbd8cb9671d9e362f43492edb2aa5a562fcb
Eunjae Lee <eunjae.lee@algolia.com>
2019-06-18 09:58:38 +0200

    chore(examples): fix snippet component

--- examples/e-commerce/src/app/snippet/snippet.component.ts
+++ examples/e-commerce/src/app/snippet/snippet.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { snippet } from 'instantsearch.js/es/helpers';
 
 @Component({
   selector: 'app-snippet',
@@ -7,41 +8,13 @@ import { Component, Input } from '@angular/core';
 export class Snippet {
   @Input() attribute: string;
   @Input() hit: { _snippetResult?: {}; label?: string; highlighted?: string };
-  @Input() tagName: string = 'em';
+  @Input() tagName: string = 'mark';
 
   get content() {
-    if (this.hit.hasOwnProperty('_snippetResult')) {
-      const attributeHighlighted = this.hit._snippetResult[this.attribute];
-
-      // check that the attributeHighlighted is a string
-      if (
-        attributeHighlighted !== undefined &&
-        typeof attributeHighlighted.value === 'string'
-      ) {
-        return this.replaceWithTagName(attributeHighlighted.value);
-      }
-    }
-
-    const fallback = this.hit[this.attribute];
-    if (!fallback) {
-      console.warn(
-        `Could not find attribute [${
-          this.attribute
-        }] into hit object, will display an empty string.`
-      );
-
-      return '';
-    }
-
-    return fallback;
-  }
-
-  replaceWithTagName(value: string) {
-    return value
-      .replace(
-        new RegExp('<em>', 'g'),
-        `<${this.tagName} class="ais-Snippet-highlighted">`
-      )
-      .replace(new RegExp('</em>', 'g'), `</${this.tagName}>`);
+    return snippet({
+      attribute: this.attribute,
+      highlightedTagName: this.tagName,
+      hit: this.hit,
+    });
   }
 }
