04ca45d4bea5180b1f2a18ce0cf960eb01b0578b
Eunjae Lee <eunjae.lee@algolia.com>
2019-06-28 17:15:41 +0200

    chore(examples): add design for empty result at e-commerce demo (#634)
    * chore(examples): add design for empty result at e-commerce demo
    
    * chore(examples): add typings NoReults component

--- examples/e-commerce/src/app/app.module.ts
+++ examples/e-commerce/src/app/app.module.ts
@@ -6,9 +6,16 @@ import { AppComponent } from './app.component';
 import { RatingMenu } from './rating-menu/rating-menu.component';
 import { Snippet } from './snippet/snippet.component';
 import { ResetFiltersMobile } from './reset-filters-mobile/reset-filters-mobile.component';
+import { NoResults } from './no-results/no-results.component';
 
 @NgModule({
-  declarations: [AppComponent, RatingMenu, Snippet, ResetFiltersMobile],
+  declarations: [
+    AppComponent,
+    RatingMenu,
+    Snippet,
+    ResetFiltersMobile,
+    NoResults,
+  ],
   imports: [BrowserModule, NgAisModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
