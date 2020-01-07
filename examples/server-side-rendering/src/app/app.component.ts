00f7d9a964d8f2586956131849c7dac0e87fdb68
Haroen Viaene <hello@haroen.me>
2020-01-07 10:52:52 +0100

    move to library

--- examples/server-side-rendering/src/app/app.component.ts
+++ examples/server-side-rendering/src/app/app.component.ts
@@ -4,28 +4,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TransferState, makeStateKey } from '@angular/platform-browser';
 import { history } from 'instantsearch.js/es/lib/routers';
 import { simple } from 'instantsearch.js/es/lib/stateMappings';
-import { createSSRSearchClient } from 'angular-instantsearch';
-import * as qs from 'qs';
-import { Routing } from 'instantsearch.js/es/lib/InstantSearch';
-
-// TODO: put this inside the library, maybe inside InstantSearch.js?
-function parseServerRequest(
-  req: { url: string } | undefined,
-  { router, stateMapping }: Routing
-) {
-  if (!req) {
-    return undefined;
-  }
-
-  // this is for faking the browser API used by the router.
-  const location = { search: `?${req.url.split('?')[1]}` };
-  // TODO: this is "private" in IS.js, but since you can change the parseURL
-  // in historyRouter, we need to use it directly
-  // we can't call .read here, since that uses browser globals, unless it can be injected
-  const routeState = router.parseURL({ qsModule: qs, location });
-
-  return stateMapping.routeToState(routeState);
-}
+import {
+  createSSRSearchClient,
+  parseServerRequest,
+} from 'angular-instantsearch';
 
 @Component({
   selector: 'app-root',
--- src/parse-server-request.ts
+++ src/parse-server-request.ts
@@ -1,13 +1,23 @@
-// import { AlgoliaSearchHelper } from 'algoliasearch-helper';
+// TODO: prevent qs leaking as a dependency, by not using parseURL, but changing .read?
+import * as qs from 'qs';
+import { Routing } from 'instantsearch.js/es/lib/InstantSearch';
 
-// Transforms url query to SearchParameters
-export function parseServerRequest(req: { url: string } | void) {
-  if (req && req.url && req.url.includes('?')) {
-    const query = req.url.split('?')[1];
-    // TODO: use routing & initialUiState here
+// TODO: put this maybe inside InstantSearch.js?
+export function parseServerRequest(
+  req: { url: string } | undefined,
+  { router, stateMapping }: Routing
+) {
+  if (!req || !req.url) {
     return undefined;
-    // return AlgoliaSearchHelper.getConfigurationFromQueryString(query);
   }
 
-  return {};
+  // this is for faking the browser API used by the router.
+  const location = { search: `?${req.url.split('?')[1]}` };
+  // TODO: this is "private" in IS.js, but since you can change the parseURL
+  // in historyRouter, we need to use it directly
+  // we can't call .read here, since that uses browser globals, unless it can be injected
+  // @ts-ignore parseURL is still private
+  const routeState = router.parseURL({ qsModule: qs, location });
+
+  return stateMapping.routeToState(routeState);
 }
