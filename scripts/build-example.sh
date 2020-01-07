d817c6d7ab998dc1fb65b1b0c612ea611153f1c6
Haroen Viaene <hello@haroen.me>
2020-01-07 10:54:44 +0100

    chore(examples): use right helper version

--- scripts/build-example.sh
+++ scripts/build-example.sh
@@ -24,5 +24,10 @@ fi
   mkdir -p ./node_modules/instantsearch.js
   cp -R ../../node_modules/instantsearch.js/* ./node_modules/instantsearch.js
 
+  # copy same helper version
+  rm -rf ./node_modules/algoliasearch-helper
+  mkdir -p ./node_modules/algoliasearch-helper
+  cp -R ../../node_modules/algoliasearch-helper/* ./node_modules/algoliasearch-helper
+
   yarn build
 )
