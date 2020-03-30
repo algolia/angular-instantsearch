c58df1890cba1f73f995236947df1686b0b8f45f
Sardorbek Pulatov <sardorbek.pulatov@vestiairecollective.com>
2020-03-30 17:46:17 +0200

    bring back old tests

--- src/__tests__/create-ssr-algolia-client.spec.ts
+++ src/__tests__/create-ssr-algolia-client.spec.ts
@@ -1,9 +1,39 @@
 import { createSSRSearchClient } from '../create-ssr-algolia-client';
 import * as algoliasearchProxy from 'algoliasearch/index';
+import { VERSION } from '../version';
+import { VERSION as AngularVersion } from '@angular/core';
 
 jest.mock('algoliasearch/index');
 
 describe('Create SSR', () => {
+  it('passes the User-Agent', () => {
+    const addAlgoliaAgent = jest.fn();
+    algoliasearchProxy.mockImplementation(() => {
+      return {
+        addAlgoliaAgent,
+      };
+    });
+
+    const ssrClient = createSSRSearchClient({
+      appId: 'test',
+      apiKey: 'test',
+      httpClient: null,
+      HttpHeaders: null,
+      makeStateKey: null,
+      transferState: null,
+    });
+    expect(addAlgoliaAgent).toHaveBeenCalledTimes(3);
+    expect(addAlgoliaAgent).toHaveBeenCalledWith(
+      `angular (${AngularVersion.full})`
+    );
+    expect(addAlgoliaAgent).toHaveBeenCalledWith(
+      `angular-instantsearch (${VERSION})`
+    );
+    expect(addAlgoliaAgent).toHaveBeenCalledWith(
+      `angular-instantsearch-server (${VERSION})`
+    );
+  });
+
   it('forwards the default options to the search client', () => {
     const addAlgoliaAgent = jest.fn();
     algoliasearchProxy.mockImplementation(() => {
