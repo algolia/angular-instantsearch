998f24e0c7ff1449f44b5b12defb4e8511debfbc
Haroen Viaene <hello@haroen.me>
2020-08-31 18:01:11 +0200

    chore: add differ

--- src/configure-related-items/configure-related-items.ts
+++ src/configure-related-items/configure-related-items.ts
@@ -1,20 +1,32 @@
-import { Component, Input, Inject, forwardRef, Optional } from '@angular/core';
+import {
+  Component,
+  Input,
+  Inject,
+  forwardRef,
+  Optional,
+  KeyValueDiffer,
+  KeyValueDiffers,
+} from '@angular/core';
 
 import { EXPERIMENTAL_connectConfigureRelatedItems } from 'instantsearch.js/es/connectors';
 import { BaseWidget } from '../base-widget';
 import { NgAisInstantSearch } from '../instantsearch/instantsearch';
 import { NgAisIndex } from '../index-widget/index-widget';
+import { AlgoliaHit } from 'instantsearch.js';
 
 @Component({
   selector: 'ais-experimental-configure-related-items',
   template: '',
 })
 export class NgAisConfigureRelatedItems extends BaseWidget {
-  @Input() public hit: object;
+  private internalHit: AlgoliaHit;
+  private differ: KeyValueDiffer<string, any>;
+
   @Input() public matchingPatterns: object;
   @Input() public transformSearchParameters: Function;
 
   constructor(
+    private differs: KeyValueDiffers,
     @Inject(forwardRef(() => NgAisIndex))
     @Optional()
     public parentIndex: NgAisIndex,
@@ -24,13 +36,35 @@ export class NgAisConfigureRelatedItems extends BaseWidget {
     super('ExperimentalConfigureRelatedItems');
   }
 
+  @Input()
+  set hit(values: AlgoliaHit) {
+    this.internalHit = values;
+    if (!this.differ && values) {
+      this.differ = this.differs.find(values).create();
+    }
+  }
+
   public ngOnInit() {
     this.createWidget(EXPERIMENTAL_connectConfigureRelatedItems, {
-      hit: this.hit,
+      hit: this.internalHit,
       matchingPatterns: this.matchingPatterns,
       transformSearchParameters: this.transformSearchParameters,
     });
 
     super.ngOnInit();
+  }
+
+  ngDoCheck() {
+    if (this.differ) {
+      const changes = this.differ.diff(this.internalHit);
+      if (changes) {
+        this.parent.removeWidgets([this.widget]);
+        this.createWidget(EXPERIMENTAL_connectConfigureRelatedItems, {
+          hit: this.internalHit,
+          matchingPatterns: this.matchingPatterns,
+          transformSearchParameters: this.transformSearchParameters,
+        });
+      }
+    }
   }
 }
