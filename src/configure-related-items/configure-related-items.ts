aa055a3991a6884c37c1a60698b5953f53d1c0ad
Haroen Viaene <hello@haroen.me>
2020-08-31 18:03:12 +0200

    chore: back to normal + types

--- src/configure-related-items/configure-related-items.ts
+++ src/configure-related-items/configure-related-items.ts
@@ -1,32 +1,26 @@
-import {
-  Component,
-  Input,
-  Inject,
-  forwardRef,
-  Optional,
-  KeyValueDiffer,
-  KeyValueDiffers,
-} from '@angular/core';
+import { Component, Input, Inject, forwardRef, Optional } from '@angular/core';
 
 import { EXPERIMENTAL_connectConfigureRelatedItems } from 'instantsearch.js/es/connectors';
 import { BaseWidget } from '../base-widget';
 import { NgAisInstantSearch } from '../instantsearch/instantsearch';
 import { NgAisIndex } from '../index-widget/index-widget';
 import { AlgoliaHit } from 'instantsearch.js';
+import { ConfigureRelatedItemsConnectorParams } from 'instantsearch.js/es/connectors/configure-related-items/connectConfigureRelatedItems';
+
+type x = ConfigureRelatedItemsConnectorParams;
 
 @Component({
   selector: 'ais-experimental-configure-related-items',
   template: '',
 })
 export class NgAisConfigureRelatedItems extends BaseWidget {
-  private internalHit: AlgoliaHit;
-  private differ: KeyValueDiffer<string, any>;
-
-  @Input() public matchingPatterns: object;
-  @Input() public transformSearchParameters: Function;
+  @Input() hit: ConfigureRelatedItemsConnectorParams['hit'];
+  @Input()
+  public matchingPatterns: ConfigureRelatedItemsConnectorParams['matchingPatterns'];
+  @Input()
+  public transformSearchParameters: ConfigureRelatedItemsConnectorParams['transformSearchParameters'];
 
   constructor(
-    private differs: KeyValueDiffers,
     @Inject(forwardRef(() => NgAisIndex))
     @Optional()
     public parentIndex: NgAisIndex,
@@ -36,35 +30,13 @@ export class NgAisConfigureRelatedItems extends BaseWidget {
     super('ExperimentalConfigureRelatedItems');
   }
 
-  @Input()
-  set hit(values: AlgoliaHit) {
-    this.internalHit = values;
-    if (!this.differ && values) {
-      this.differ = this.differs.find(values).create();
-    }
-  }
-
   public ngOnInit() {
     this.createWidget(EXPERIMENTAL_connectConfigureRelatedItems, {
-      hit: this.internalHit,
+      hit: this.hit,
       matchingPatterns: this.matchingPatterns,
       transformSearchParameters: this.transformSearchParameters,
     });
 
     super.ngOnInit();
-  }
-
-  ngDoCheck() {
-    if (this.differ) {
-      const changes = this.differ.diff(this.internalHit);
-      if (changes) {
-        this.parent.removeWidgets([this.widget]);
-        this.createWidget(EXPERIMENTAL_connectConfigureRelatedItems, {
-          hit: this.internalHit,
-          matchingPatterns: this.matchingPatterns,
-          transformSearchParameters: this.transformSearchParameters,
-        });
-      }
-    }
   }
 }
