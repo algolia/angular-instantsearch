6a45385580e10b57193ba42b55b6422d3c27df69
Fabien Motte <fabien.motte@algolia.com>
2022-01-26 17:19:47 +0100

    chore: update shipjs config to work with master (#893)

--- ship.config.js
+++ ship.config.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-  mergeStrategy: { toSameBranch: ['develop'] },
+  mergeStrategy: { toSameBranch: ['master'] },
   shouldPrepare: ({ releaseType, commitNumbersPerType }) => {
     const { fix = 0 } = commitNumbersPerType;
     if (releaseType === 'patch' && fix === 0) {
