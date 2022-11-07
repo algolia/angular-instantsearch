6acc7cf662ab5103b3cf411e2cc00f573d6f6257
F. Levi <55688616+flevi29@users.noreply.github.com>
2022-11-07 15:21:32 +0200

    fix: forgot to add padding to connector options

--- src/pagination/pagination.ts
+++ src/pagination/pagination.ts
@@ -151,6 +151,7 @@ export class NgAisPagination extends TypedBaseWidget<
     this.createWidget(
       connectPagination,
       {
+        padding: parseNumberInput(this.padding),
         totalPages: parseNumberInput(this.totalPages),
       },
       {
